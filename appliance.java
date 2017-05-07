57866e7d66df6e4fb3d76ad915c89f5110155eb6
kea07 <kea07@mail.aub.edu>
2017-05-07 20:16:27 +0300

    Add files via upload

--- appliance.java
+++ appliance.java
@@ -2,69 +2,64 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 import java.lang.*;
+
 //This class is used as a database
-public class appliance
-{
-    public String name;
-    public ArrayList<String> FixList=new ArrayList<>();
-    public ArrayList<Integer> FixCostList=new ArrayList<>();
-    public ArrayList<Integer> stockCost=new ArrayList<>();
-    public ArrayList<Integer> stockCount=new ArrayList<>();
-    public ArrayList<String>  stockName=new ArrayList<>();
+public class appliance {
+	public String name;
+	public ArrayList<String> FixList = new ArrayList<>();
+	public ArrayList<Integer> FixCostList = new ArrayList<>();
+	public ArrayList<Integer> stockCost = new ArrayList<>();
+	public ArrayList<Integer> stockCount = new ArrayList<>();
+	public ArrayList<String> stockName = new ArrayList<>();
 
-    public void configureFromFile(String FILENAME) throws NullPointerException, FileNotFoundException {
-        Scanner reader=new Scanner(new File(FILENAME));
-        String temp;
-        temp=reader.nextLine();
-        name=temp;
-        int result;
-        temp=reader.nextLine();
-        int n=0;
-        while(!temp.equals("endFixName") )
-        {
-            FixList.add(n,temp);
-            n++;
-            temp=reader.nextLine();
-        }
-        temp=reader.nextLine();
-        n=0;
-        while(!temp.equals("endFixCost"))
-        {
+	public void configureFromFile(String FILENAME) throws NullPointerException, FileNotFoundException {
+		Scanner reader = new Scanner(new File(FILENAME));
+		String temp;
+		temp = reader.nextLine();
+		name = temp;
+		int result;
+		temp = reader.nextLine();
+		int n = 0;
+		while (!temp.equals("endFixName")) {
+			FixList.add(n, temp);
+			n++;
+			temp = reader.nextLine();
+		}
+		temp = reader.nextLine();
+		n = 0;
+		while (!temp.equals("endFixCost")) {
 
-            result = Integer.parseInt(temp);
-            FixCostList.add(n,result);
-            n++;
-            temp=reader.nextLine();
-        }
-        temp=reader.nextLine();
-        n=0;
-        while(!temp.equals("endStockName"))
-        {
-            stockName.add(n,temp);
-            n++;
-            temp=reader.nextLine();
+			result = Integer.parseInt(temp);
+			FixCostList.add(n, result);
+			n++;
+			temp = reader.nextLine();
+		}
+		temp = reader.nextLine();
+		n = 0;
+		while (!temp.equals("endStockName")) {
+			stockName.add(n, temp);
+			n++;
+			temp = reader.nextLine();
 
-        }
-        temp=reader.nextLine();
-        n=0;
-        while(!temp.equals("endStockCount"))
-        {
-            result = Integer.parseInt(temp);
-            stockCount.add(n,result);
-            n++;
-            temp=reader.nextLine();
-            
-        }
-        temp=reader.nextLine();
-        n=0;
-        while(!temp.equals("endStockCost"))
-        {
-            result = Integer.parseInt(temp);
-            stockCost.add(n,result);
-            n++;
-            temp=reader.nextLine();
+		}
+		temp = reader.nextLine();
+		n = 0;
+		while (!temp.equals("endStockCount")) {
+			result = Integer.parseInt(temp);
+			stockCount.add(n, result);
+			n++;
+			temp = reader.nextLine();
 
-        }
-    }
+		}
+		temp = reader.nextLine();
+		n = 0;
+		while (!temp.equals("endStockCost")) {
+			result = Integer.parseInt(temp);
+			stockCost.add(n, result);
+			n++;
+			temp = reader.nextLine();
+
+		}
+	}
 
 }
--- customer.java
+++ customer.java
@@ -5,53 +5,48 @@ import java.io.*;
 
 public class customer {
 
-    public ArrayList<String> accountSummary = new ArrayList<>();
-    public Integer ID;
-    public ArrayList<Integer> stockCount = new ArrayList<>();
-    public ArrayList<Integer> stockThreshold =new ArrayList<>();
-    public ArrayList<String> stockName =new ArrayList<>();
-    public Integer TotalCost = 0;
+	public ArrayList<String> accountSummary = new ArrayList<>();
+	public Integer ID;
+	public ArrayList<Integer> stockCount = new ArrayList<>();
+	public ArrayList<Integer> stockThreshold = new ArrayList<>();
+	public ArrayList<String> stockName = new ArrayList<>();
+	public Integer TotalCost = 0;
 
-    public void configureFromFile(String FILENAME) throws NullPointerException, FileNotFoundException
-    {
-        Scanner reader=new Scanner(new File(FILENAME));
-        String temp;
-        temp=reader.nextLine();
-        ID=Integer.parseInt(temp);
-        temp=reader.nextLine();
-        while(!temp.equals("endStockCount") )
-        {
-            stockCount.add(Integer.parseInt(temp));
-            temp=reader.nextLine();
-        }
-        temp=reader.nextLine();
-        while(!temp.equals("endStockThreshold"))
-        {
+	public void configureFromFile(String FILENAME) throws NullPointerException, FileNotFoundException {
+		Scanner reader = new Scanner(new File(FILENAME));
+		String temp;
+		temp = reader.next();
+		ID = Integer.parseInt(temp);
+		temp = reader.next();
+		while (!temp.equals("endStockCount")) {
+			stockCount.add(Integer.parseInt(temp));
+			temp = reader.next();
+		}
+		temp = reader.next();
+		while (!temp.equals("endStockThreshold")) {
 
-            stockThreshold.add(Integer.parseInt(temp));
-            temp=reader.nextLine();
-        }
-        temp=reader.nextLine();
-        while(!temp.equals("endStockName"))
-        {
-            stockName.add(temp);
-            temp=reader.nextLine();
-        }
-    }
+			stockThreshold.add(Integer.parseInt(temp));
+			temp = reader.next();
+		}
+		temp = reader.next();
+		while (!temp.equals("endStockName")) {
+			stockName.add(temp);
+			temp = reader.next();
+		}
+	}
 
-    public void addFix(String FixName) {
-        accountSummary.add(FixName);
-    }
+	public void addFix(String FixName) {
+		accountSummary.add(FixName);
+	}
 
-    public Boolean consumeStock(String app, Integer count)
-    {
-        count=stockCount.get(stockName.indexOf(app)) - count;
-        stockCount.set(stockName.indexOf(app),count);
-        return true;
-    }
-    public void addStock(String app, Integer count)
-    {
-        stockCount.set(stockName.indexOf(app),count);
-        accountSummary.add(app);
-    }
+	public Boolean consumeStock(String app, Integer count) {
+		count = stockCount.get(stockName.indexOf(app)) - count;
+		stockCount.set(stockName.indexOf(app), count);
+		return true;
+	}
+
+	public void addStock(String app, Integer count) {
+		stockCount.set(stockName.indexOf(app), count);
+		accountSummary.add(app);
+	}
 }
